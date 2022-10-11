222c4776878495066e82043a988841793bf0b0c2
sharmakamaldesign <sharma.kamal.design@gmail.com>
2022-10-11 10:57:22 +0530

    unit testing

--- test/chat.e2e-spec.ts
+++ test/chat.e2e-spec.ts
@@ -5,6 +5,7 @@ import { AppModule } from './../src/app.module';
 import { Sequelize } from 'sequelize-typescript';
 import { databaseProviders } from '../src/common/database/database.provider';
 import {
+    admin_token,
    token
 } from './test-data';
 
@@ -19,7 +20,8 @@ class TestLogger implements LoggerService {
 
 describe('Slot Module', () => {
     let app: INestApplication;
-
+    let newGroupId;
+    let newChatIt;
     beforeAll(async () => {
         const module = await Test.createTestingModule({
             imports: [AppModule],
@@ -50,6 +52,30 @@ describe('Slot Module', () => {
         .then((res) => {
             let body = res.body;
             console.log("login unit test body====>", body);
+            expect(body).toBeDefined();
+            expect(201)
+        });
+    });
+
+    it('create User', async () => {
+
+        let obj ={
+            "name": "Kabir",
+            "role_id": "612722b3-b8db-43eb-9db5-d0448b9d17f4",
+            "email": "kabir.@gmail.com",
+            "password": "test1234",
+            "gender": "male",
+            "mobile": "0000000005"
+          }
+
+        const res = await request(app.getHttpServer())
+        .post(`/user`)
+        .send(obj)
+        .set('Authorization', `Bearer ${admin_token}`)
+        .expect(201)
+        .then((res) => {
+            let body = res.body;
+            console.log("create User test body====>", body);
             expect(body).toBeDefined();
             expect(201)
         });
@@ -100,166 +126,157 @@ describe('Slot Module', () => {
         });
     });
 
-//     it('update doctor', async () => {
+    it('create chat group', async () => {
 
-//         let obj = {
-//                 "reg_no": "test2",
-//                 "name": "test",
-//                 "birth_year": 1997,
-//                 "dob": "string",
-//                 "practicing_since": 7,
-//                 "qualification": "string",
-//                 "reg_council": "string",
-//                 "id":"bdc69955-d692-40f4-8a2d-f7a02588c67d"
-              
-//         }
+        let obj ={
+            "name": "New Year",
+            "code": "new_year2010",
+          }
 
-//         const res = await request(app.getHttpServer())
-//         .post(`/doctor/update`)
-//         .send(obj)
-//         .set('Authorization', `Bearer ${token}`)
-//         .expect(201)
-//         .then((res) => {
-//             let body = res.body;
-//             console.log("update doctor unit test body====>", body);
-//             expect(body).toBeDefined();
-//             expect(body.statusCode).toEqual(200)
-//         });
-//     });
-
-//     it('add-speciality-mapping to doctor', async () => {
-
-//         let obj = {
-//             "speciality_id": "0eeedcd4-4913-4982-b0d8-62a3b073ac63",
-//             "doctor_id": "bdc69955-d692-40f4-8a2d-f7a02588c67d"
-//         }
-              
-//         const res = await request(app.getHttpServer())
-//         .post(`/doctor/add-speciality-mapping`)
-//         .send(obj)
-//         .set('Authorization', `Bearer ${token}`)
-//         .expect(201)
-//         .then((res) => {
-//             let body = res.body;
-//             console.log("update doctor unit test body====>", body);
-//             expect(body).toBeDefined();
-//             expect(body.statusCode).toEqual(200)
-//         });
-//     });
-
-//     it('get_doctor_list', async () => {
-
-//         const res = await request(app.getHttpServer())
-//         .get(`/doctor`)
-//         .set('Authorization', `Bearer ${token}`)
-//         .expect(200)
-//         .then((res) => {
-//             let body = res.body;
-//             console.log("doctor list====>", body);
-//             expect(body).toBeDefined();
-//             expect(body.statusCode).toEqual(200)
-//         });
-//     });
-
-//     //TODO issue in add-my-speciality-mapping API
-//     // it('add-my-speciality-mapping to doctor', async () => {
-//     //     let obj = {
-//     //         "speciality_id": "0eeedcd4-4913-4982-b0d8-62a3b073ac63"
-//     //     }
-              
-//     //     const res = await request(app.getHttpServer())
-//     //     .post(`/doctor/add-my-speciality-mapping`)
-//     //     .send(obj)
-//     //     .set('Authorization', `Bearer ${token}`)
-//     //     .expect(201)
-//     //     .then((res) => {
-//     //         let body = res.body;
-//     //         console.log("add-my-speciality-mappint unit test body====>", body);
-//     //         expect(body).toBeDefined();
-//     //         expect(body.statusCode).toEqual(200)
-//     //     });
-//     // });
+        const res = await request(app.getHttpServer())
+        .post(`/chat/group`)
+        .send(obj)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(201)
+        .then((res) => {
+            let body = res.body;
+            newGroupId = body.id
+            console.log("create chat grout test body====>", body);
+            expect(body).toBeDefined();
+            expect(201)
+        });
+    });
 
 
-//     it('add-lang-mapping to doctor', async () => {
-//         let obj = {
-//             "name": "Hindi",
-//             "doctor_id": "bdc69955-d692-40f4-8a2d-f7a02588c67d"
-//         }
-              
-//         const res = await request(app.getHttpServer())
-//         .post(`/doctor/add-lang-mapping`)
-//         .send(obj)
-//         .set('Authorization', `Bearer ${token}`)
-//         .expect(201)
-//         .then((res) => {
-//             let body = res.body;
-//             console.log("add-lang-mapping unit test body====>", body);
-//             expect(body).toBeDefined();
-//             expect(body.statusCode).toEqual(200)
-//         });
-//     });
-    
-//     //TODO issue in this API
-//     // it('add-my-lang-mapping to doctor', async () => {
-//     //     let obj = {
-//     //         "name": "Hindi"
-//     //     }
-              
-//     //     const res = await request(app.getHttpServer())
-//     //     .post(`/doctor/add-my-lang-mapping`)
-//     //     .send(obj)
-//     //     .set('Authorization', `Bearer ${token}`)
-//     //     .expect(201)
-//     //     .then((res) => {
-//     //         let body = res.body;
-//     //         console.log("add-my-lang-mapping unit test body====>", body);
-//     //         expect(body).toBeDefined();
-//     //         expect(body.statusCode).toEqual(200)
-//     //     });
-//     // });
+    it('get chat groups', async () => {
 
+        const res = await request(app.getHttpServer())
+        .get(`/chat/group`)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200)
+        .then((res) => {
+            let body = res.body;
+            console.log("get chat groups unit test body====>", body);
+            expect(body).toBeDefined();
+            expect(200)
+        });
+    });
 
-//     it('get-search-autocomplete to doctor', async () => {
-//         let search_term ="radiol"
-//         const res = await request(app.getHttpServer())
-//         .get(`/doctor/get-search-autocomplete?search_term=${search_term}`)
-//         .set('Authorization', `Bearer ${token}`)
-//         .expect(200)
-//         .then((res) => {
-//             let body = res.body;
-//             console.log("get-search-autocomplete unit test body====>", body);
-//             expect(body).toBeDefined();
-//             expect(body.statusCode).toEqual(200)
-//         });
-//     });
+    it('add user to chat group', async () => {
 
+        let obj ={
+            "user_id": "89182a1f-b7c0-454b-8136-babe897e575a",
+            "group_id": newGroupId
+          }
 
-//     it('get doctor by id', async () => {
-//         let doctor_id ="bdc69955-d692-40f4-8a2d-f7a02588c67d"
-//         const res = await request(app.getHttpServer())
-//         .get(`/doctor/${doctor_id}`)
-//         .set('Authorization', `Bearer ${token}`)
-//         .expect(200)
-//         .then((res) => {
-//             let body = res.body;
-//             console.log("get doctor by id unit test body====>", body);
-//             expect(body).toBeDefined();
-//             expect(body.statusCode).toEqual(200)
-//         });
-//     });
+        const res = await request(app.getHttpServer())
+        .post(`/chat/group/user`)
+        .send(obj)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(201)
+        .then((res) => {
+            let body = res.body;
+            console.log("add user to chat group test body====>", body);
+            expect(body).toBeDefined();
+            expect(201)
+        });
+    });
 
-//     //TODO need to understand
-//     // it('get-my-profile (doctor)', async () => {
-//     //     const res = await request(app.getHttpServer())
-//     //     .get(`/doctor/get-my-profile`)
-//     //     .set('Authorization', `Bearer ${token}`)
-//     //     .expect(200)
-//     //     .then((res) => {
-//     //         let body = res.body;
-//     //         console.log("get-my-profile (doctor) unit test body====>", body);
-//     //         expect(body).toBeDefined();
-//     //         expect(body.statusCode).toEqual(200)
-//     //     });
-//     // });
+    it('send chat message', async () => {
+
+        let obj ={
+            "message": "Hello there!",
+            "group_id": newGroupId
+          }
+        
+        console.log("send chat message "+obj.message, obj.group_id);
+
+        const res = await request(app.getHttpServer())
+        .post(`/chat`)
+        .send(obj)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(201)
+        .then((res) => {
+            let body = res.body;
+            newChatIt = body.id;
+            console.log("send chat message test body====> ", body);
+            expect(body).toBeDefined();
+            expect(201)
+        });
+    });
+
+    it('get users of a chat group', async () => {
+        console.log("new group id: "+newGroupId);
+        const res = await request(app.getHttpServer())
+        .get(`/chat/group/users/`+newGroupId)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200)
+        .then((res) => {
+            let body = res.body;
+            console.log("get users of a chat group unit test body====>", body);
+            expect(body).toBeDefined();
+            expect(200)
+        });
+    });
+
+    it('get chats of group', async () => {
+
+        const res = await request(app.getHttpServer())
+        .get(`/chat/`+newGroupId)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200)
+        .then((res) => {
+            let body = res.body;
+            console.log("get chats of groups unit test body====>", body);
+            expect(body).toBeDefined();
+            expect(200)
+        });
+    });
+
+    it('like chat message', async () => {
+
+        let obj ={
+            "chat_id": newChatIt,
+          }
+        
+        const res = await request(app.getHttpServer())
+        .post(`/chat/like`)
+        .send(obj)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(201)
+        .then((res) => {
+            let body = res.body;
+            console.log("like chat message test body====> ", body);
+            expect(body).toBeDefined();
+            expect(201)
+        });
+    });
+
+    it('get likes of a chat', async () => {
+
+        const res = await request(app.getHttpServer())
+        .get(`/chat/like/`+newChatIt)
+        .set('Authorization', `Bearer ${token}`)
+        .expect(200)
+        .then((res) => {
+            let body = res.body;
+            console.log("get likes of a chat unit test body====>", body);
+            expect(body).toBeDefined();
+            expect(200)
+        });
+    });
+
+    // it('delete chat group', async () => {
+    //     console.log("new chat group id"+newGroupId);
+    //     const res = await request(app.getHttpServer())
+    //     .delete(`/chat/group/`+newGroupId)
+    //     .set('Authorization', `Bearer ${token}`)
+    //     .expect(200)
+    //     .then((res) => {
+    //         let body = res.body;
+    //         console.log("delete chat grout test body====>", body);
+    //         expect(body).toBeDefined();
+    //         expect(200)
+    //     });
+    // });
 });
